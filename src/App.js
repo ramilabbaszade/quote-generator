b7f789a9c953a8f09f32434c861320c33adacd70
ramilabbaszade <abbasovramil65@gmail.com>
2020-12-21 19:15:44 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getQuote();
+  }, [setQuotes, setIsLoading]);
+
+  const getQuote = () => {
+    fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuotes(data.data);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="restart_btn" onClick={getQuote}>
+        Restart
+      </div>
+      <div className="App_container">
+        {quotes.map((quotes) => {
+          return (
+            <main key={quotes._id}>
+              <div className="quote">
+                <p>{isLoading ? "Loading..." : quotes.quoteText}</p>
+              </div>
+              <div className="author">
+                {isLoading ? "Loading..." : quotes.quoteAuthor}
+              </div>
+              <small>{quotes.quoteGenre}</small>
+            </main>
+          );
+        })}
+      </div>
     </div>
   );
 }
